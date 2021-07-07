94fa60b16a1fa485dda21afbf76afafdaf6e24a2
quangkm-1085 <khuat.minh.quang@sun-asterisk.com>
2021-07-07 17:04:22 +0700

    Home Page

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <h1>{{ $t('home') }}</h1>
+    <h1 class="text-home">{{ $t('home') }}</h1>
   </div>
 </template>
 
@@ -9,3 +9,20 @@ export default {
   name: 'Home'
 }
 </script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Otomanopee+One&display=swap');
+body {
+  background-image: url("../assets/background.jpg");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+}
+.text-home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 5rem;
+  color: yellow;
+  font-family: 'Otomanopee One', sans-serif;
+}
+</style>
